a9a23cc1094eb731a2672a0d0fb4b5c0bc0af84d
Alade Abraham <aladeoluwasola20@gmail.com>
2026-09-09 10:42:58 +0100

    Done with this

--- Septmeber/2.5ASS.js
+++ Septmeber/2.5ASS.js
@@ -11,15 +11,21 @@ let pictures = [
     "images/item-5.png",
 ];
 
+let colors = ['blue', 'red', 'green', 'yellow', 'orange'];
+
+
+
 let currentPicture = 0;
 
+let body = document.body;
 let picture = document.getElementById("picture");
 let nextBtn = document.getElementById("nextBtn");
 
 
 function changePicture() {
     currentPicture += 1;
-
+    body.style.backgroundColor = colors[currentPicture];
+    
     if (currentPicture == pictures.length) {
         currentPicture = 0;
     }
@@ -30,6 +36,8 @@ function changePicture() {
 nextBtn.addEventListener("click", changePicture);
 
 setInterval(changePicture, 5000);
+
+
 
 // let message = document.querySelector(".message");
 // let messages = ['Welcome to my website!', 'Enjoy your stay!', 'Check out our latest updates!', 'Feel free to explore!', 'Contact us for more information!'];
@@ -48,3 +56,8 @@ setInterval(changePicture, 5000);
 // setInterval(() => {
 //     message.textContent = "Welcome to my website!";
 // }, 5000);
+
+
+// Create a counter manually using a button to increase the count by 1 and display it on the screen. Do it manually increase the count by 1 every 5 seconds.
+// There should be a button to reset the count back to 0.
+// When the number is positive the counter should be green, when it is negative it should be red, and when it is 0 it should be black.
